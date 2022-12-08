91e8ceff39b8ca9ce8143e0f2104f441329c487c
Adam Schlicht <aschlicht@student.unimelb.edu.au>
2022-12-08 11:18:25 +1100

    update reduce_avro.py

--- reduce_avro.py
+++ reduce_avro.py
@@ -8,13 +8,15 @@ schema = avro.schema.parse(open(schema_file, "rb").read())
 big_avro = input("Original avro file: ")
 reader = DataFileReader(open(big_avro, "rb"), DatumReader())
 
+num_rows = input("Number of rows: ")
+
 writer = DataFileWriter(open(big_avro.replace(".avro", "_small.avro"), "wb"), DatumWriter(), schema)
 
 count=0
 for row in reader:
     writer.append(row)
     count+=1
-    if count > 0:
+    if count > num_rows-1:
         break
 
 writer.close()
